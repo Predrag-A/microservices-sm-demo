63191eae15e69a6157db535128db9c0f21d1a177
Predrag Antanasijevic <predragantanasijevic@Predrags-MacBook-Pro.local>
2021-09-01 10:47:20 +0200

    Added service registry Eureka server

---
+++ service-registry/src/test/java/com/predrag/a/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predrag.a.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
